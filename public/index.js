8920cf1cee2287155fdc97b217f0f40e89a7e8ac
mj majadly <majadly@nevada.unr.edu>
2020-11-04 22:02:40 +0200

    data

--- public/index.js
+++ public/index.js
@@ -46,3 +46,58 @@ document.querySelector('.searchBox').addEventListener('keyup', (event) => {
     lastNamesIncludesInputText = [...namesIncludesInputText];
   }
 });
+
+const form = document.querySelector('form');
+const output = document.querySelector('output');
+
+form.addEventListener('submit', (event) => {
+  // stop the form submitting and reloading the page
+  event.preventDefault();
+
+  // clear out any previous results
+  output.innerHTML = '';
+
+  // get the value of the field with name="pokemon"
+  const formData = new FormData(event.target);
+  const name = formData.get('kidname');
+  console.log(formData);
+  // request that pokemon from PokeAPI
+  console.log(name);
+
+  fetch(`https://gender-api.com/get?name=${name}&key=YeFJcGQluraWPyRCsP`)
+    .then((response) => {
+      if (!response.ok) throw new Error(response.status);
+      return response.json();
+    })
+    // if we get a successful response
+    .then((data) => {
+      const heading = document.createElement('h2');
+      const { gender } = data;
+      heading.textContent = data.name;
+      const headingtwo = document.createElement('h3');
+      headingtwo.textContent = gender;
+      const image = document.createElement('img');
+      if (gender === 'male') {
+        image.src = '/public/img/boy.jpg';
+        image.alt = '';
+      } else if (gender === 'female') {
+        image.src = './img/girl.jpg';
+        image.alt = '';
+      } else {
+        image.src = './img/boygirl.jpg';
+        image.alt = '';
+      }
+
+      output.appendChild(heading);
+      output.appendChild(headingtwo);
+      output.appendChild(image);
+    })
+    // if the request is unsuccessful
+    .catch((error) => {
+      if (error.message === '404') {
+        output.textContent = `⚠️ Couldn't find "${name}"`;
+      } else {
+        output.textContent = '⚠️ Something went wrong';
+      }
+    });
+});
