f537f44fde3a3e3398f497acac4e8d6114054804
nizarhalloun <nshalloun@hotmail.com>
2020-11-05 13:05:38 +0200

    Fixed #5

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const port = process.env.PORT || 3000;
 
 const hostname = process.env.HOSTNAME || 'localhost';
 const server = http.createServer(router);
-// eslint-disable-next-line no-console
-console.log(`Listening on http://${hostname}:${port}`);
 
-server.listen(port);
+server.listen(port).on('listening', () => {
+  // eslint-disable-next-line no-console
+  console.log(`Listening on http://${hostname}:${port}`);
+});
