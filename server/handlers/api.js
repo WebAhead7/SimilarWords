3b4ed03d6eb17ab0a0d1eaf85051ab122432006a
rakad-kh <69585198+rakad-kh@users.noreply.github.com>
2020-11-12 03:41:36 +0200

    Update api.js

--- server/handlers/api.js
+++ server/handlers/api.js
@@ -3,7 +3,7 @@ const url = require('url');
 
 module.exports = (req, link) => new Promise((resolve, reject) => {
     const name = url.parse(req.url, true);
-    https.get(`link`, (res) => {
+    https.get(link, (res) => {
         let new_data = "";
         res.on("data", chunk => {
             new_data += chunk;
