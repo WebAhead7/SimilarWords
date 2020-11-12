216bdab66021de7fdbf0525cf2c34ce5a7099877
rakad-kh <69585198+rakad-kh@users.noreply.github.com>
2020-11-12 03:00:17 +0200

    Update inputHandler.js

--- server/handlers/inputHandler.js
+++ server/handlers/inputHandler.js
@@ -22,9 +22,7 @@ function inputHandler(req, res) {
         .then(({ data }) => {
             const final_data = new_arr(JSON.parse(data));
             res.end(JSON.stringify(final_data))
-        }).catch(err => {
-
-        })
+        }).catch(err => (err => alert(error.message))
 }
 
 module.exports = inputHandler;
