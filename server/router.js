05d64be1d3dad2f1cde7423c0a46b6de2559c06d
rakad-kh <69585198+rakad-kh@users.noreply.github.com>
2020-11-13 00:26:03 +0200

    Update router.js

--- server/router.js
+++ server/router.js
@@ -15,9 +15,9 @@ const router = (req, res) => {
     homeHandler(req, res);
   } else if (url.includes('public')) {
     publicHandler(req, res);
-  } else if (url.includes('/autoComplete')) {
+  } else if (url.includes('autoComplete')) {
     dataHandler(req, autocomplete_funcion, autocomplete_api, res);
-  } else if (url.includes('/similarWords')) {
+  } else if (url.includes('similarWords')) {
     dataHandler(req, similarwords_funcion, similar_words_api, res);
   } else {
     missingHandler(req, res);
