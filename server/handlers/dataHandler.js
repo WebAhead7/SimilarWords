29c3f31d89aea6d9afb4d7be49a45356d2cca0fd
rakad-kh <69585198+rakad-kh@users.noreply.github.com>
2020-11-12 03:43:03 +0200

    Update dataHandler.js

--- server/handlers/dataHandler.js
+++ server/handlers/dataHandler.js
@@ -3,7 +3,7 @@ const url = require('url');
 const fetchApi = require('./api');
 
 function dataHandler(req, res, new_arr) {
-    fetchApi(req)
+    fetchApi(req, `https://api.datamuse.com/sug?s=${name.query.name}`)
         .then(({ data }) => {
             const final_data = new_arr(JSON.parse(data));
             res.end(JSON.stringify(final_data))
